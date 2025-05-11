a8bb309aefd0a202f5604cd4d3cbdddc9d6341b3
dhymaaaa <dheemahussain44@gmail.com>
2025-05-11 21:16:46 +0500

    scrollable chat

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -349,7 +349,7 @@ const Home = () => {
       )}
       <div className="flex h-screen bg-[#292929] text-white">
         {/* Sidebar */}
-        <div className="w-80 bg-[#1E1E1E] flex flex-col">
+        <div className="h-full w-80 bg-[#1E1E1E] flex flex-col flex-shrink-0">
           <div className="p-4 flex items-center justify-between">
             <button className="p-2">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#E9D8B5">
@@ -431,10 +431,10 @@ const Home = () => {
             <span className="text-sm ml-3">{userData.email || 'Loading...'}</span>
           </div>
         </div>
-
-        {/* Main content */}
-        <div className="flex-1 flex flex-col">
-          <div className="flex justify-between items-center p-4">
+        {/* Main content area */}
+        <div className="flex-1 flex flex-col h-screen">
+          {/* Fixed Header */}
+          <div className="flex justify-between items-center p-4 bg-[#292929]">
             <div className="flex">
               <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
                 width="80pt" height="45pt" viewBox="0 0 310.000000 178.000000"
@@ -447,64 +447,66 @@ const Home = () => {
                 <g transform="translate(0.000000,178.000000) scale(0.100000,-0.100000)"
                   fill="#E9D8B5" stroke="none">
                   <path d="M2445 1370 c0 -13 -47 -16 -345 -20 -433 -6 -412 2 -424 -172 -7 -107 -5 -112
-                      49 -84 39 20 85 1 85 -34 0 -12 -24 -29 -76 -54 -63 -30 -75 -39 -69 -54 4 -9
-                      11 -35 15 -57 14 -70 51 -88 205 -98 l40 -2 3 -77 c2 -43 6 -78 10 -78 4 0 43
-                      36 87 80 l80 80 245 0 c262 0 277 3 313 51 17 24 19 44 19 213 0 128 -3 195
-                      -12 216 -13 32 -55 60 -89 60 -33 0 -51 11 -51 31 0 27 68 26 121 -2 69 -35
-                      79 -70 79 -294 0 -178 -1 -194 -22 -235 -38 -74 -56 -78 -338 -86 l-245 -6
-                      -111 -110 c-73 -72 -116 -108 -124 -103 -9 6 -11 33 -8 102 6 115 1 123 -80
-                      123 -34 0 -71 7 -92 18 -41 20 -80 83 -80 131 0 27 -3 32 -17 27 -24 -9 -167
-                      -113 -219 -159 -120 -106 23 73 156 194 l80 74 0 107 c0 59 5 119 11 135 12
-                      33 56 82 84 93 11 5 178 8 370 7 304 -2 350 -4 350 -17z m-111 -212 c29 -41 1
-                      -124 -62 -190 -36 -38 -62 -49 -62 -26 0 6 18 40 41 75 45 68 48 104 7 82 -13
-                      -7 -53 -36 -89 -65 -96 -76 -159 -114 -191 -114 -42 0 -33 22 20 47 26 13 80
-                      51 121 85 101 87 160 128 182 128 10 0 25 -10 33 -22z m-1590 -106 c40 -24 66
-                      -77 66 -137 0 -72 -25 -107 -102 -143 -60 -29 -213 -61 -226 -48 -3 3 1 6 10
-                      6 18 0 186 86 211 107 48 44 41 157 -10 169 -27 7 -53 -14 -53 -43 0 -16 -12
-                      -17 -28 -1 -16 16 -15 60 1 80 32 40 73 44 131 10z m441 7 c77 -36 107 -136
-                      66 -218 -16 -31 -33 -43 -89 -69 -63 -28 -158 -52 -205 -52 -11 0 24 23 78 50
-                      116 59 130 71 146 117 14 42 -2 107 -28 117 -25 10 -53 -3 -53 -25 0 -24 -29
-                      -37 -41 -18 -18 29 -9 67 22 93 37 31 47 32 104 5z m63 -391 c-3 -27 -12 -37
-                      -63 -66 -33 -19 -105 -71 -159 -115 -125 -99 -139 -106 -167 -81 -23 20 -23
-                      12 3 179 12 74 15 80 37 80 37 0 48 -26 32 -82 -20 -70 -24 -103 -14 -103 5 0
-                      52 36 103 80 98 84 182 138 213 139 15 1 18 -5 15 -31z m-600 -40 c2 -21 5
-                      -38 8 -38 2 0 27 18 55 40 57 45 83 50 99 20 13 -24 4 -60 -13 -60 -7 0 -39
-                      -27 -72 -60 -44 -44 -67 -60 -87 -60 -29 0 -48 23 -48 59 0 42 -24 33 -88 -33
-                      -69 -71 -95 -79 -100 -33 -3 23 10 41 75 107 80 80 112 101 148 97 15 -2 21
-                      -11 23 -39z m1047 25 c21 -21 15 -85 -15 -142 -35 -69 -130 -130 -130 -84 0 9
-                      18 43 40 74 22 32 40 62 40 68 0 17 -24 13 -46 -7 -45 -40 -151 -114 -210
-                      -144 -59 -31 -64 -32 -80 -16 -17 17 -12 54 6 43 15 -9 41 8 158 102 59 48
-                      121 95 137 105 37 22 78 23 100 1z"
+                49 -84 39 20 85 1 85 -34 0 -12 -24 -29 -76 -54 -63 -30 -75 -39 -69 -54 4 -9
+                11 -35 15 -57 14 -70 51 -88 205 -98 l40 -2 3 -77 c2 -43 6 -78 10 -78 4 0 43
+                36 87 80 l80 80 245 0 c262 0 277 3 313 51 17 24 19 44 19 213 0 128 -3 195
+                -12 216 -13 32 -55 60 -89 60 -33 0 -51 11 -51 31 0 27 68 26 121 -2 69 -35
+                79 -70 79 -294 0 -178 -1 -194 -22 -235 -38 -74 -56 -78 -338 -86 l-245 -6
+                -111 -110 c-73 -72 -116 -108 -124 -103 -9 6 -11 33 -8 102 6 115 1 123 -80
+                123 -34 0 -71 7 -92 18 -41 20 -80 83 -80 131 0 27 -3 32 -17 27 -24 -9 -167
+                -113 -219 -159 -120 -106 23 73 156 194 l80 74 0 107 c0 59 5 119 11 135 12
+                33 56 82 84 93 11 5 178 8 370 7 304 -2 350 -4 350 -17z m-111 -212 c29 -41 1
+                -124 -62 -190 -36 -38 -62 -49 -62 -26 0 6 18 40 41 75 45 68 48 104 7 82 -13
+                -7 -53 -36 -89 -65 -96 -76 -159 -114 -191 -114 -42 0 -33 22 20 47 26 13 80
+                51 121 85 101 87 160 128 182 128 10 0 25 -10 33 -22z m-1590 -106 c40 -24 66
+                -77 66 -137 0 -72 -25 -107 -102 -143 -60 -29 -213 -61 -226 -48 -3 3 1 6 10
+                6 18 0 186 86 211 107 48 44 41 157 -10 169 -27 7 -53 -14 -53 -43 0 -16 -12
+                -17 -28 -1 -16 16 -15 60 1 80 32 40 73 44 131 10z m441 7 c77 -36 107 -136
+                66 -218 -16 -31 -33 -43 -89 -69 -63 -28 -158 -52 -205 -52 -11 0 24 23 78 50
+                116 59 130 71 146 117 14 42 -2 107 -28 117 -25 10 -53 -3 -53 -25 0 -24 -29
+                -37 -41 -18 -18 29 -9 67 22 93 37 31 47 32 104 5z m63 -391 c-3 -27 -12 -37
+                -63 -66 -33 -19 -105 -71 -159 -115 -125 -99 -139 -106 -167 -81 -23 20 -23
+                12 3 179 12 74 15 80 37 80 37 0 48 -26 32 -82 -20 -70 -24 -103 -14 -103 5 0
+                52 36 103 80 98 84 182 138 213 139 15 1 18 -5 15 -31z m-600 -40 c2 -21 5
+                -38 8 -38 2 0 27 18 55 40 57 45 83 50 99 20 13 -24 4 -60 -13 -60 -7 0 -39
+                -27 -72 -60 -44 -44 -67 -60 -87 -60 -29 0 -48 23 -48 59 0 42 -24 33 -88 -33
+                -69 -71 -95 -79 -100 -33 -3 23 10 41 75 107 80 80 112 101 148 97 15 -2 21
+                -11 23 -39z m1047 25 c21 -21 15 -85 -15 -142 -35 -69 -130 -130 -130 -84 0 9
+                18 43 40 74 22 32 40 62 40 68 0 17 -24 13 -46 -7 -45 -40 -151 -114 -210
+                -144 -59 -31 -64 -32 -80 -16 -17 17 -12 54 6 43 15 -9 41 8 158 102 59 48
+                121 95 137 105 37 22 78 23 100 1z"
                   />
                 </g>
               </svg>
             </div>
-            <div className="flex-1 flex justify-end">
-              <button className="p-2 mb-4">
+            <div className="flex justify-end">
+              <button className="p-2">
                 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
                   width="15pt" height="15pt" viewBox="0 0 512.000000 512.000000"
                   preserveAspectRatio="xMidYMid meet">
                   <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)"
                     fill="#E9D8B5" stroke="none">
                     <path d="M1810 5071 c-381 -125 -719 -327 -1006 -604 -232 -223 -388 -434
-                        -526 -710 -501 -1007 -310 -2193 483 -2988 502 -504 1199 -781 1899 -756 548
-                        20 1015 179 1455 496 472 339 815 837 975 1415 35 128 36 147 11 203 -26 57
-                        -84 87 -156 81 -44 -4 -64 -13 -119 -54 -158 -116 -259 -179 -381 -239 -283
-                        -138 -527 -195 -839 -195 -515 0 -993 197 -1356 559 -367 367 -565 841 -564
-                        1356 1 443 131 839 380 1158 63 81 81 134 65 189 -23 76 -93 129 -168 127 -21
-                        0 -90 -18 -153 -38z m-284 -573 c-78 -187 -133 -397 -156 -598 -14 -123 -14
-                        -410 0 -525 94 -767 564 -1432 1245 -1764 314 -154 585 -219 940 -228 356 -9
-                        626 42 964 184 57 24 106 41 109 39 8 -8 -120 -213 -199 -319 -440 -591 -1099
-                        -927 -1819 -927 -505 0 -948 147 -1354 448 -275 205 -510 490 -669 810 -159
-                        321 -229 624 -229 987 1 315 45 554 154 830 163 413 462 784 833 1032 71 48
-                        212 132 222 133 1 0 -18 -46 -41 -102z"/>
+                  -526 -710 -501 -1007 -310 -2193 483 -2988 502 -504 1199 -781 1899 -756 548
+                  20 1015 179 1455 496 472 339 815 837 975 1415 35 128 36 147 11 203 -26 57
+                  -84 87 -156 81 -44 -4 -64 -13 -119 -54 -158 -116 -259 -179 -381 -239 -283
+                  -138 -527 -195 -839 -195 -515 0 -993 197 -1356 559 -367 367 -565 841 -564
+                  1356 1 443 131 839 380 1158 63 81 81 134 65 189 -23 76 -93 129 -168 127 -21
+                  0 -90 -18 -153 -38z m-284 -573 c-78 -187 -133 -397 -156 -598 -14 -123 -14
+                  -410 0 -525 94 -767 564 -1432 1245 -1764 314 -154 585 -219 940 -228 356 -9
+                  626 42 964 184 57 24 106 41 109 39 8 -8 -120 -213 -199 -319 -440 -591 -1099
+                  -927 -1819 -927 -505 0 -948 147 -1354 448 -275 205 -510 490 -669 810 -159
+                  321 -229 624 -229 987 1 315 45 554 154 830 163 413 462 784 833 1032 71 48
+                  212 132 222 133 1 0 -18 -46 -41 -102z"/>
                   </g>
                 </svg>
               </button>
             </div>
           </div>
-          <div className="flex-1 flex flex-col">
-            <div className="flex-1 overflow-y-auto p-6">
+
+          {/* Scrollable Middle Content */}
+          <div className="flex-1 overflow-y-auto">
+            <div className="h-full p-6">
               {isAuthenticated ? (
                 messages.length === 0 ? (
                   // Empty state for chat
@@ -528,8 +530,8 @@ const Home = () => {
                       >
                         <div
                           className={`max-w-[70%] rounded-lg p-4 ${message.role === 'user'
-                              ? 'bg-[#E9D8B5] text-black'
-                              : 'bg-[#1E1E1E] text-white'
+                            ? 'bg-[#E9D8B5] text-black'
+                            : 'bg-[#1E1E1E] text-white'
                             }`}
                         >
                           {message.content}
@@ -567,35 +569,35 @@ const Home = () => {
                 </div>
               )}
             </div>
+          </div>
 
-            {/* Chat input */}
-            <div className="p-6">
-              <form onSubmit={handleSubmit} className="relative w-full max-w-3xl mx-auto">
-                <input
-                  type="text"
-                  className="w-full px-4 py-3 bg-transparent text-[#E9D8B5] placeholder-[#E9D8B5] border border-[#E9D8B5] rounded-full focus:outline-none focus:ring-1 focus:ring-[#E9D8B5]"
-                  placeholder="Type a message"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  disabled={!isAuthenticated || isProcessing}
-                />
-                <button
-                  type="submit"
-                  disabled={!isAuthenticated || isProcessing || !message.trim()}
-                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#E9D8B5] hover:text-white transition-colors disabled:opacity-50"
-                >
-                  {isProcessing ? (
-                    <Icon path={mdiLoading} size={1} className="animate-spin" />
-                  ) : (
-                    <Icon className='text-[#E9D8B5]' path={mdiArrowRight} size={1} />
-                  )}
-                </button>
-              </form>
-              <div className="flex justify-center text-xs text-gray-400 mt-2">
-                <div className='font-bold'>Akuru can make mistakes</div>
-                <div className='font-bold ml-5'>...</div>
-                <div className='font-bold ml-5'>އަކުރަށް ގޯސްކޮށްވެސް ބުނެވިދާނެ</div>
-              </div>
+          {/* Fixed Bottom Input */}
+          <div className="p-6 bg-[#292929]">
+            <form onSubmit={handleSubmit} className="relative w-full max-w-3xl mx-auto">
+              <input
+                type="text"
+                className="w-full px-4 py-3 bg-transparent text-[#E9D8B5] placeholder-[#E9D8B5] border border-[#E9D8B5] rounded-full focus:outline-none focus:ring-1 focus:ring-[#E9D8B5]"
+                placeholder="Type a message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                disabled={!isAuthenticated || isProcessing}
+              />
+              <button
+                type="submit"
+                disabled={!isAuthenticated || isProcessing || !message.trim()}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-[#E9D8B5] hover:text-white transition-colors disabled:opacity-50"
+              >
+                {isProcessing ? (
+                  <Icon path={mdiLoading} size={1} className="animate-spin" />
+                ) : (
+                  <Icon className='text-[#E9D8B5]' path={mdiArrowRight} size={1} />
+                )}
+              </button>
+            </form>
+            <div className="flex justify-center text-xs text-gray-400 mt-2">
+              <div className='font-bold'>Akuru can make mistakes</div>
+              <div className='font-bold ml-5'>...</div>
+              <div className='font-bold ml-5'>އަކުރަށް ގޯސްކޮށްވެސް ބުނެވިދާނެ</div>
             </div>
           </div>
         </div>
